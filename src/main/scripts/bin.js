2dd58eb7d485dd5b948b8c91b471507840b98845
Rubem <rubemfsv15@gmail.com>
2022-07-16 19:47:29 -0300

    fix: fix project name creation

--- src/main/scripts/bin.js
+++ src/main/scripts/bin.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 const { execSync } = require('child_process');
-const yargs = require('yargs');
 
 const runCommand = (command) => {
   try {
@@ -13,7 +12,8 @@ const runCommand = (command) => {
   return true;
 };
 
-const repoName = yargs.argv[2];
+const repoName = process.argv[2];
+
 const gitCheckoutCommand = `git clone --depth 1 https://github.com/rubemfsv/clean-react-app ${repoName}`;
 const installDependenciesCommand = `cd ${repoName} && npm install`;
 
