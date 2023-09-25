df55e7c50cbf795afb05983ada43b2b77ce6a76b
Rubem Vasconcelos <rubemfsv15@gmail.com>
2023-09-25 14:24:39 -0300

    chore: delete src/presentation/components/Field directory due to not use this

--- src/presentation/components/Field/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Styles from './styles.scss';
-
-const Field = (props) => {
-  return <div className={Styles.field}>{props.children}</div>;
-};
-
-export default Field;
