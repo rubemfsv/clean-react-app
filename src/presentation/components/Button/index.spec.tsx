a37f17f0172a15c8947a1077ac07acc4fb9560c0
coldrohak <rjrohak@gmail.com>
2023-10-09 23:07:54 +0530

    fix: final commit

--- src/presentation/components/Button/index.spec.tsx
+++ src/presentation/components/Button/index.spec.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render, fireEvent, screen } from '@testing-library/react'
 import { Button, IButtonProps } from '@/presentation/components'
-import faker from 'faker';
+import faker from 'faker'
 
 const makeSut = (props: IButtonProps) => {
   render(<Button {...props} />)
